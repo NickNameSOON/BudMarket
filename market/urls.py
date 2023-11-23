a477a09f3ef4c46ee7f6701fbda0cdd838ce6e69
NickNameSoon <volodymyr.r.savchuk@ukd.edu.ua>
2023-11-24 01:52:20 +0200

    commit

--- market/urls.py
+++ market/urls.py
@@ -6,4 +6,5 @@ app_name = "market"
 urlpatterns = [
     path('', views.index),
     path('<int:product_id>/', views.IndexProduct, name="detail"),
+    path('addproduct', views.add_product, name="add_product"),
 ]
--- market/views.py
+++ market/views.py
@@ -2,17 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 
+
 # Create your views here.
 def index(request):
-   products = Product.objects.all()
-   context = {
-       'products': products
-   }
-   return render(request, "market/index.html", context)
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, "market/index.html", context)
+
 
 def IndexProduct(request, product_id):
     product = Product.objects.get(id=product_id)
-    context = {
-        'product': product
-    }
-    return render(request, "market/detail.html", context)
+    context = {'product': product}
+    return render(request, "market/detail.html", context=context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES['upload']
+        Product = Product(name=name, price=price, description=description, image=image)
+        Product.save()
+
+    return render(request, "market/addproduct.html")
