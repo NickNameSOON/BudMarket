51d1e91fd25d7422ddc8a6232d016a7424562b23
nicknamesoon <volodymyr.r.savchuk@ukd.edu.ua>
2024-05-14 01:42:09 +0300

    commit

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from market import views
-from market.views import SearchResultsView
 
 app_name = "market"
 
@@ -10,6 +9,4 @@ urlpatterns = [
     path('product/<str:product_id>', views.product_detail, name="detail"),
     path('catalog/', views.catalog, name='catalog'),
     path('catalog/<str:category_slug>/', views.catalog, name='catalog_by_category'),
-    path('catalog/search/', views.catalog_search, name='catalog_search'),  # змінено URL
-    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Product
 from cart.forms import CartAddProductForm
-from django.db.models import Count
+from django.db.models import Count, Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import logging
-from django.views.generic import ListView
-from django.db.models import Q
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -30,8 +31,6 @@ def product_detail(request, product_id):
     return render(request, 'market/detail.html', context={'product': product, 'form': form})
 
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 def catalog(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -40,6 +39,8 @@ def catalog(request, category_slug=None):
     selected_category_slug = request.GET.get('category')
     selected_brands = request.GET.getlist('brand')
     sort_param = request.GET.get('sort')
+    query = request.GET.get('q', '')
+
     products = Product.objects.filter(available=True)
 
     if selected_category_slug:
@@ -54,6 +55,9 @@ def catalog(request, category_slug=None):
     elif sort_param == 'max_price':
         products = products.order_by('-price')
 
+    if query:
+        products = products.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
     # Setup paginator
     paginator = Paginator(products, 20)  # Відображати 20 товарів на сторінку
     page = request.GET.get('page')
@@ -61,51 +65,20 @@ def catalog(request, category_slug=None):
     try:
         products = paginator.page(page)
     except PageNotAnInteger:
-        # Якщо сторінка не є числом, відображати першу сторінку.
         products = paginator.page(1)
     except EmptyPage:
-        # Якщо номер сторінки більший, ніж загальна кількість сторінок, відображати останню сторінку.
         products = paginator.page(paginator.num_pages)
 
     return render(request, 'market/catalog.html', {
         'category': category,
         'categories': categories,
         'brands': brands,
-        'products': products
+        'products': products,
+        'selected_brands': selected_brands,
+        'sort_param': sort_param,
+        'query': query,
     })
-
-
-def catalog_search(request):
-    query = request.GET.get('q', '')
-    sort_by = request.GET.get('sort_by', None)
-
-    logger.debug(f"Query: {query}, Sort by: {sort_by}")
-
-    if query:
-        products = Product.objects.filter(title__icontains=query)
-        logger.debug(f"Found products: {products.count()}")
-    else:
-        products = Product.objects.all()
-
-    if sort_by == 'price_asc':
-        products = products.order_by('price')
-    elif sort_by == 'price_desc':
-        products = products.order_by('-price')
-
-    return render(request, 'market/products_list.html', {'products': products})
 
 
 def aboutUs(request):
     return render(request, 'market/about_us.html')
-
-
-class SearchResultsView(ListView):
-    model = Product
-    template_name = 'market/products_list.html'  # переконайтесь, що це правильний шаблон
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
-        else:
-            return Product.objects.none()
