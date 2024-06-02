888fb727e9ac6be3f48b86f38755b76264022066
nicknamesoon <volodymyr.r.savchuk@ukd.edu.ua>
2024-06-02 13:01:10 +0300

    commit

--- tailwind.config-Laptop.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./market/**/*.{html,js}", "./users/**/*.{html,js}", "./cart/**/*.{html,js}", "./order/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
